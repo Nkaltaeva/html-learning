a233ec3c8f02d658a4c8bc063d7f7a40cb80b5aa
kistochka <tusich@bk.ru>
2025-03-26 14:11:04 +0300

    new carousel/scroll

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,73 +1,9 @@
-// Получаем переменные
-let arrowLeft = document.getElementById("arrow-left"),
-  arrowRight = document.getElementById("arrow-right"),
-  sliderImg = document.getElementById("carousel-window"),
-  dotsWrap = document.getElementsByClassName("main_dots-navigation")[0],
-  dots = document.getElementsByClassName("dot"),
-  left = 0,
-  imgWidth = "520px";
+const carousel = document.querySelector(".carousel");
+const slide = document.querySelector(".carousel-watch-photo");
 
-// Функция перелистывания слайдов
-slideTo = (direction) => {
-  if (direction == "left") left -= imgWidth;
-  if (direction == "right") left += imgWidth;
-  if (left < -1040) left = 0;
-  if (left > 0) left = -1040;
-
-  for (let item of dots) {
-    item.classList.remove("active");
-  }
-  switch (left) {
-    case 0:
-      dots[0].classList.add("active");
-      break;
-    case -520:
-      dots[1].classList.add("active");
-      break;
-    case -1040:
-      dots[2].classList.add("active");
-      break;
-  }
-  sliderImg.style.left = left + "px";
-};
-
-// Добавляем события клика на кнопки
-arrowLeft.addEventListener("click", () => {
-  clearInterval(timerId);
-  slideTo("left");
-});
-
-arrowRight.addEventListener("click", () => {
-  clearInterval(timerId);
-  slideTo("right");
-});
-
-// Добавляем события клика на точки
-dotsWrap.addEventListener("click", (event) => {
-  let target = event.target;
-
-  if (target.classList.contains("dot")) {
-    for (let item of dots) {
-      item.classList.remove("active");
-    }
-    target.classList.add("active");
-    switch (target) {
-      case dots[0]:
-        left = 0;
-        break;
-      case dots[1]:
-        left = -520;
-        break;
-      case dots[2]:
-        left = -1040;
-        break;
-      case dots[3]:
-        left = -1560;
-        break;
-      case dots[4]:
-        left = -2080;
-        break;
-    }
-    sliderImg.style.left = left + "px";
-  }
-});
+function handleCarouselMove(positive = true) {
+  const slideWidth = slide.clientWidth;
+  carousel.scrollLeft = positive
+    ? carousel.scrollLeft + slideWidth
+    : carousel.scrollLeft - slideWidth;
+}
--- sliderBar/sliderBar.js
+++ sliderBar/sliderBar.js
@@ -1,4 +1,5 @@
 // slider
+
 function openNav() {
   document.getElementById("bigSliderBar-content").style.display = "block";
   document.getElementById("smallSliderBar-content").style.display = "none";
@@ -7,4 +8,18 @@ function openNav() {
 function closeNav() {
   document.getElementById("bigSliderBar-content").style.display = "none";
   document.getElementById("smallSliderBar-content").style.display = "block";
+}
+
+// scrollbar
+const scrollTopBtnBig = document.getElementById("scroll-topBtnBig");
+const scrollTopBtnSmall = document.getElementById("scroll-topBtnSmall");
+const scrollElementDOM = document.documentElement;
+
+scrollTopBtnBig.addEventListener("click", scrollBtn);
+scrollTopBtnSmall.addEventListener("click", scrollBtn);
+function scrollBtn() {
+  scrollElementDOM.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
